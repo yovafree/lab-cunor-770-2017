7dcbc8e2d9f1bfc9971cd4aa3c9ee99874842398
Geovani de León <erwingeo1021@gmail.com>
2017-02-21 08:43:21 -0600

    Práctica 1 de JAVA

---
+++ practica5 - java/jaPractica1/src/clases/principal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Yova
+ */
+public class principal {
+    static int num1;
+    static int num2;
+    
+    // Tipos de datos
+    // int Enteros
+    // char caracteres solo acepta 1 caracter
+    // String para cadenas de texto
+    // Boolean para verdaderos y falsos
+    // Double para punto flotante
+    // float para punto flotante doble precisión
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        num1= 4;
+        num2= 8;
+        float resu = num1 / num2;
+        System.out.println("La sumna es: " + resu);
+    }
+    
+}
